ad6d6f7985f327a5080f380f48c9847a4802c34f
Hari Narayan Singh <rs22201227@gmail.com>
2024-02-07 14:42:11 +0530

    js

---
+++ JavaScript/Operation.js
@@ -0,0 +1,34 @@
+/**
+ * Assignment operator
+ */
+
+// var name = "raman"
+// console.log(name)
+
+/**
+ * Arithematic operator
+ */
+// console.log(4+5)
+// var i=5
+// console.log(i++)
+
+// var i=5
+// console.log(++i)
+
+/**
+ * logical operator
+ */
+
+// console.log(3>4 && true)
+
+// console.log(3>4 || true)
+
+// console.log(true^true)
+
+/**
+ * Bitwise operator
+ */
+
+// console.log(10 & 6)
+
+// console.log(10|6)
