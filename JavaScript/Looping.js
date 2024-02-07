3eb66bdfef2e77762d84e6742b588c7bab8380df
Hari Narayan Singh <rs22201227@gmail.com>
2024-02-08 00:48:56 +0530

    js

---
+++ JavaScript/Looping.js
@@ -0,0 +1,43 @@
+/**
+ * FOR LOOP
+ */
+
+// for(var i=0 ; i<10 ; i++){
+//     console.log("hello baby")
+// }
+
+/**
+ * WHILE LOOP
+ */
+// var i=0
+// while(i<10){
+//     console.log("hello bchha")
+//     i++
+// }
+
+//---------- continue and break statement --------------------------------------------
+
+// for(var i=0 ; i<=50 ; i++){
+//     if(i%2!=0){
+//         continue;
+//     }
+//     console.log(i)
+
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   for (var j = 0; j < 10; j++) {
+//     if (j % 2 != 0) {
+//       continue;
+//     }
+//     console.log(j)
+//   }
+// }
+
+
+for(var i=0 ; i<10; i++){
+    if(i==5){
+        break;
+    }
+    console.log("hey "+ i + " love you")
+}
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+if(num%2==0){
+//     console.log("even")
+// }
+// else
+// console.log("odd")
