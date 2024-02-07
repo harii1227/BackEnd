81ffb2e21fbb00925a8a44f6cf6d28f31f183214
Hari Narayan Singh <rs22201227@gmail.com>
2024-02-07 14:23:03 +0530

    javascript

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,41 @@
+// PRIMITIVE DATATYPES
+
+
+// string------------------
+
+// var name="raman"
+// var clas = 'twelve'
+// console.log(clas)
+// console.log(typeof name)
+
+
+// Number---------------------
+
+// var age=12
+// var x=1.4
+// console.log(typeof x)
+// var newno=123n
+// console.log(typeof newno)
+// console.log(typeof NaN)
+
+//null Data Types-----------------
+
+// console.log(typeof null)
+// var name
+// console.log(typeof name)
+
+
+// symbol data types---------------
+
+// var s1= symbol()
+
+// non premitive data types
+
+// object
+
+var person={
+    name:"raman",
+    age:19,
+    hobby:"dance"
+}
+console.log(person)
---
+++ JavaScript/variables.js
@@ -0,0 +1,12 @@
+var name
+
+console.log(name)
+
+var age=19
+console.log(age)
+
+var age
+var nationality
+
+// var %StaticRange     error naming rule
+// console.log(%state)
