9bc21d125b217ab7a62bf0db96073ecf0eff76a5
Hari Narayan Singh <rs22201227@gmail.com>
2024-02-11 19:43:13 +0530

    js

---
+++ JavaScript/Day-2/Function.js
@@ -0,0 +1,19 @@
+/**
+ * FUNCTION
+ */
+
+// function hellostudents(){
+//     console.log("hello students !")
+// }
+// hellostudents()
+
+
+/**
+ * i wat two sum two numbeer 
+ */
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(5,6));
